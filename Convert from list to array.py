d537230fc473b9ef17f86529b67e119301e8d07a
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-01 18:58:41 +0600

    Update Convert from list to array.py

--- Convert from list to array.py
+++ Convert from list to array.py
@@ -1,2 +1,9 @@
 array1 = list(name1)
 array2 = list(name2)
+
+# No need to split again, as they are already lists of characters
+first_array = array1
+second_array = array2
+
+print("First array is:", first_array)
+print("Second array is:", second_array)
