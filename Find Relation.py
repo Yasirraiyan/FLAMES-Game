565e2e1a291edd237c6fa1257715ee27a7101f4b
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-01 19:12:24 +0600

    Create Find Relation.py

---
+++ Find Relation.py
@@ -0,0 +1,16 @@
+def find_relation:
+if valid:
+if length == 1:
+    print("Friend")
+elif length == 2:
+    print("Love")
+elif length == 3:
+    print("Affection")
+elif length == 4:
+    print("Marriage")
+elif length == 5:
+    print("Enemy")
+elif length == 6:
+    print("Siblings")
+else:
+    print("No specific relationship defined for this number of common elements.")
